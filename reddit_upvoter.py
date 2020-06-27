31806f5a290b66ab7a4f9d06c3ed273da5625d51
allen44 <allen44@users.noreply.github.com>
2020-06-26 15:18:51 -1000

    Added login and upvote functions to script

--- reddit_upvoter.py
+++ reddit_upvoter.py
@@ -1,13 +1,19 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
 from sys import argv
 
 #Add your own reddit credentials
-assert argv[1]
-assert argv[2]
-reddit_username = argv[1]
-reddit_password = argv[2]
+try:
+    reddit_username = argv[1]
+    reddit_password = argv[2]
+except:
+    reddit_username = input('Reddit username: ')
+    reddit_password = input('Reddit password: ')
+
 
 options = Options()
 # options.add_argument("--headless")
@@ -17,29 +23,54 @@ options.add_argument("--incognito")
 
 chrome_browser = webdriver.Chrome('./chromedriver', chrome_options=options)
 chrome_browser.maximize_window()
-chrome_browser.get('https://www.seleniumeasy.com/test/basic-first-form-demo.html')
+chrome_browser.get('https://www.reddit.com/login/')
 
-assert 'Selenium Easy Demo' in chrome_browser.title
+# #First login in to reddit before we are allowed to upvote
+# login_button = chrome_browser.find_element(By.LINK_TEXT, 'Log in')
 
- # Check to see if the "Show Message" buttonexists on page
-assert 'Show Message' in chrome_browser.page_source
+#Enter username
+username_field = chrome_browser.find_element(By.ID, 'loginUsername')
+username_field.clear()
+username_field.send_keys(reddit_username)
 
-user_message = chrome_browser.find_element(By.ID, 'user-message')
-user_message.clear()
-user_message.send_keys('I am cool!')
+#Enter password
+password_field = chrome_browser.find_element(By.ID, 'loginPassword')
+password_field.clear()
+password_field.send_keys(reddit_password)
 
-user_button2 = chrome_browser.find_element(By.CSS_SELECTOR, '#get-input > .btn')
-print(user_button2)
+#Click Log In button
+login_button = chrome_browser.find_element(By.CLASS_NAME, 'AnimatedForm__submitButton')
+login_button.click()
 
-show_message_button = chrome_browser.find_element(By.CLASS_NAME, 'btn-default')
-show_message_button.click()
+# try:
+print('starting to wait up to 10 seconds')
+element = WebDriverWait(chrome_browser, 10).until(
+    EC.presence_of_element_located((By.CLASS_NAME, "icon-upvote"))
+)
+print('ending wait')
 
-output_message = chrome_browser.find_element(By.ID, 'display')
-assert 'I am cool!' in output_message.text
+#We identify the upvote buttons by the intersection fo two CSS selectors
+def intersection(lst1, lst2): 
+    lst3 = [value for value in lst1 if value in lst2] 
+    return lst3 
 
-chrome_browser.find_element(By.ID, 'display')
 
-chrome_browser.get('reddit')
-chrome_browser.quit()
+css_selector1= chrome_browser.find_elements(By.CSS_SELECTOR, "button[aria-label='upvote']")
+css_selector2 = chrome_browser.find_elements(By.CSS_SELECTOR, 'button[aria-pressed="false"]')
+upvote_buttons = intersection(css_selector1, css_selector2)
+
+wait_counter = 5
+for upvote_button in upvote_buttons:
+    print("upvoting")
+    # upvote_button.click()
+    hover = ActionChains(chrome_browser).move_to_element(upvote_button)
+    hover.perform()
+    upvote_button.click()
+    chrome_browser.implicitly_wait(wait_counter)
+    print(f'waiting {wait_counter} seconds until next upvote')
+    wait_counter=+ wait_counter
+# except:
+    # chrome_browser.quit()
+    # print('quit early')
 
 print('script complete')
